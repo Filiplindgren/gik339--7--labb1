b09ba144a15ae3aabafb5ff231cd95ebd30c1196
MonaTlili <113024784+MonaTlili@users.noreply.github.com>
2023-12-04 12:00:14 +0100

    sista push

--- script.js
+++ script.js
@@ -3,76 +3,82 @@
 // Hämta checkboxen
 const htmlCheckbox = document.querySelector('.checkbox');
 
-//console.log(htmlCheckbox);
 
 // Uppgift 2: Hämta alla textfält som en samling, hämtar en htmlCollection
 
 const allTextfieldElements = document.getElementsByClassName('textfield');
-//console.log(allTextfieldElements);
 
 // Hämta upp samlingen av textfält, hämtar en NodeList
 
 const allTextfields = document.querySelectorAll('.textfield');
-//console.log(allTextfields);
-//console.log(allTextfields);
+
 
 // Uppgift 3:  Hämta knappen
 
 const htmlButton = document.getElementById('form_button');
-//console.log(htmlButton);
+
 
 // Uppgift 4: Hämta div-elementet 
 
 let htmlDiv = document.querySelector('.form_div');
-//console.log(htmlDiv);
+
 
 // Uppgift 5
 
 // Hämta textfält med klassen content
 let contentInput = document.getElementById('content');
 
-// Funktion för att visa keyboardinput i textfält och form_div
-function showInput (e) {
-    htmlDiv.innerHTML = contentInput.value;
-    // Logga elementet som startade eventet
+
+ // Funktion som används för båda textfälten för det som ska hända när man interagerar med dem på hemsidan, div ändrar färg, text skrivs ut i div
+ // Elementen som startar eventet loggas också 
+ function showInputAllTextfields(e) {
+    // Hämta textfälten
+    const colorInputField = allTextfields.item(0);
+    const contentInputField = allTextfields.item(1);
+
+    // Hämta deras name attirbut och lagra i en variabel
+    const contentInputName = contentInputField.getAttribute('name');
+    const colorInputName = colorInputField.getAttribute('name');
+
+    // Logga eventobjektet
     console.log(e);
+    // Om namnet är 'content' så ska innehålet i den skrivas ut i diven
+    // Hämta inputfältets färg
+    const divColor = colorInputField.innerHTML.value;
+    
+    // 
+    htmlDiv.style.backgroundColor = divColor;
+    // Text från textfält 'innehåll' ska skrivas ut i diven 
+    htmlDiv.innerHTML = contentInput.value;
+
  }
 
+ 
  // Sätter en eventlistener på textfältet
-contentInput.addEventListener('input', showInput);
+contentInput.addEventListener('input', showInputAllTextfields); 
 
 // Uppgift 6 
 // hämta inputfält för färg
 let colorInput = document.getElementById('color');
 
-// funktion för att logga element som startar eventet
-function showColorInput (e) {
-    // Logga elementet som startade eventet
-    console.log(e);
-}
 
 
-
-// Sätter en eventlyssnare på checkboxen
-htmlCheckbox.addEventListener('change',changeDivColor);
+// Sätter en eventlyssnare på checkboxen med en anonym funktion 
+// För att ändra färg i diven
+htmlCheckbox.addEventListener('change', () => {
+     // Hämta färg från inputfield
+     const divColor = allTextfields.item(0).value;
+     // sätt färgen som hämtats som bakgrund i diven 
+     htmlDiv.style.backgroundColor = divColor;
+});
 
 // Logga element som startar eventet
-colorInput.addEventListener('input',showColorInput);
+colorInput.addEventListener('input',showInputAllTextfields);
 
 
-// Funktion för att ändra färg på form_div när man trycker i checkboxen
-function changeDivColor (e) { 
-    // Hämta färg från inputfield
-    const divColor = allTextfields.item(0).value;
-    // sätt färgen som hämtats som bakgrund i diven 
-    htmlDiv.style.backgroundColor = divColor;
-}
-
-
-// Function för att gömma form_div
+// Function för att ta bort form_div
 htmlButton.addEventListener('click', () => {
-
-    htmlDiv.style.display = 'none';
+    htmlDiv.remove();
 });
 
 
